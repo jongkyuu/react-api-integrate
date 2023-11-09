617ba6895daf75c1aa3fdbb97165d474bf7b5092
minerva421 <minerva421.ai@gmail.com>
2023-11-10 01:53:21 +0900

    API 연동

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Users from "./components/Users";
 
 function App() {
-  return (
-    <div className="App">
-      <Users></Users>
-    </div>
-  );
+    return <Users />;
 }
 
 export default App;
