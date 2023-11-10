9d047fdba122691452a504c09979688788d12498
jongkyuu <minerva421.ai@gmail.com>
2023-11-10 21:26:15 +0900

    Custom Hook 개발중

---
+++ src/components/useAsync.js
@@ -0,0 +1,52 @@
+import { useReducer, useEffect, useCallback } from "react";
+
+// loading, success, error
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      };
+    case "SUCCESS":
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      };
+    default:
+      throw new Error(`Unhandled Action Type : ${action.type}`);
+  }
+}
+
+export default function useAsync(callback, deps = []) {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
+
+  const fetchData = useCallback(async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const data = await callback();
+      dispatch({ type: "SUCCESS", data });
+    } catch (e) {
+      dispatch({ type: "ERROR", error: e });
+    }
+  }, [callback]);
+
+  useEffect(() => {
+    fetchData();
+    //eslint-disable-next-line
+  }, deps);
+
+  return [state, fetchData];
+}
